f8d15dda88164b26b44786e2037b33e80f3af7c0
Kim Inseo <82724012+Kim-Inseo@users.noreply.github.com>
2024-10-25 05:33:07 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+
+queue = deque()
+
+N = int(sys.stdin.readline().rstrip())
+
+for i in range(N):
+    order = sys.stdin.readline().rstrip()
+    
+    if order[:2] == 'pu':
+        push, num = order.split()
+        queue.append(num)
+    elif order == 'pop':
+        try:
+            num = queue.popleft()
+            print(num)
+        except:
+            print(-1)
+    elif order == 'size':
+        print(len(queue))
+    elif order == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order == 'front':
+        try:
+            num = queue[0]
+            print(num)
+        except:
+            print(-1)
+    elif order == 'back':
+        try:
+            num = queue[-1]
+            print(num)
+        except:
+            print(-1)
